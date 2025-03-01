6a883cd9bef9afd7beeead238f53066333973478
adag21044 <adag21044@gmail.com>
2025-03-01 14:28:54 +0300

    movement fixed

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -2,18 +2,23 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Hareket hızı
+    public float moveSpeed = 5f; 
+    private Rigidbody rb;
 
-    void Update()
+    void Start()
     {
-        // Input ile WASD tuşlarını okuyoruz
-        float moveX = Input.GetAxis("Horizontal"); // A ve D tuşları (X ekseni)
-        float moveZ = Input.GetAxis("Vertical");   // W ve S tuşları (Z ekseni)
-
-        // Hareket vektörü oluşturuyoruz
-        Vector3 move = new Vector3(moveX, 0, moveZ) * moveSpeed * Time.deltaTime;
-
-        // Küpün pozisyonunu güncelliyoruz
-        transform.Translate(move);
+        rb = GetComponent<Rigidbody>(); // Rigidbody bileşenini al
     }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal"); 
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0, moveZ) * moveSpeed * Time.fixedDeltaTime;
+
+        // Rigidbody kullanarak hareketi uygula
+        rb.MovePosition(rb.position + move);
+    }
+
 }
